af118b6b69228d0e3a0cfcb30cf01449aefdec36
Andrew Osmond <aosmond@mozilla.com>
2019-09-23 13:15:10 -0400

    fixup

--- gfx/2d/Swizzle.cpp
+++ gfx/2d/Swizzle.cpp
@@ -5,7 +5,6 @@
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
 #include "Swizzle.h"
-#include "SwizzleImpl.h"
 #include "Logging.h"
 #include "Tools.h"
 #include "mozilla/CheckedInt.h"
