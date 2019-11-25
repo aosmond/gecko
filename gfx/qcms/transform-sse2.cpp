742b8dca7c6bb850c803f6a68260fdd3acd12227
Andrew Osmond <aosmond@mozilla.com>
2019-11-25 14:41:28 -0500

    initial sse2 tetra impl (not verified)

--- gfx/qcms/transform-sse2.cpp
+++ gfx/qcms/transform-sse2.cpp
@@ -160,126 +160,191 @@ void qcms_transform_data_bgra_out_lut_sse2(const qcms_transform *transform,
   qcms_transform_data_template_lut_sse2<BGRA_R_INDEX, BGRA_G_INDEX, BGRA_B_INDEX, BGRA_A_INDEX>(transform, src, dest, length);
 }
 
+#define CLU_OFFSET(x, y, z) \
+	offset_##x[0] + offset_##y[1] + offset_##z[2]
+
 // Using lcms' tetra interpolation algorithm.
-template <size_t kRIndex, size_t kGIndex, size_t kBIndex, size_t kAIndex = NO_A_INDEX>
+template <size_t kSwapRB, size_t kAIndex = NO_A_INDEX>
 static void qcms_transform_data_template_tetra_clut_sse2(const qcms_transform *transform, const unsigned char *src, unsigned char *dest, size_t length) {
 	const unsigned int components = A_INDEX_COMPONENTS(kAIndex);
 	unsigned int i;
 	int xy_len = 1;
 	int x_len = transform->grid_size;
 	int len = x_len * x_len;
-	float* r_table = transform->r_clut;
+	float* r_table = kSwapRB ? transform->b_clut : transform->r_clut;
 	float* g_table = transform->g_clut;
-	float* b_table = transform->b_clut;
-	float c0_r, c1_r, c2_r, c3_r;
-	float c0_g, c1_g, c2_g, c3_g;
-	float c0_b, c1_b, c2_b, c3_b;
-	float clut_r, clut_g, clut_b;
+	float* b_table = kSwapRB ? transform->r_clut : transform->b_clut;
+
+	uint32_t offset_l[4];
+	uint32_t offset_h[4];
+
+	unsigned char in_a;
+	int p0, p1, p2, p3;
+
+	__m128i px, xx, xn;
+	__m128 rr, rrcmp;
+	__m128 cr0, cr1, cg0, cg1, cb0, cb1;
 	for (i = 0; i < length; i++) {
-		unsigned char in_r = src[kRIndex];
-		unsigned char in_g = src[kGIndex];
-		unsigned char in_b = src[kBIndex];
-		unsigned char in_a;
+		// Load in a single pixel and place each component into the
+		// lower 16-bit words. 
+		px = _mm_loadu_si32(src);
 		if (kAIndex != NO_A_INDEX) {
 			in_a = src[kAIndex];
 		}
+
 		src += components;
-		float linear_r = in_r/255.0f, linear_g=in_g/255.0f, linear_b = in_b/255.0f;
+		px = _mm_unpacklo_epi8(px, px);
+		px = _mm_srli_epi16(px, 8);
 
-		int x = in_r * (transform->grid_size-1) / 255;
-		int y = in_g * (transform->grid_size-1) / 255;
-		int z = in_b * (transform->grid_size-1) / 255;
-		int x_n = int_div_ceil(in_r * (transform->grid_size-1), 255);
-		int y_n = int_div_ceil(in_g * (transform->grid_size-1), 255);
-		int z_n = int_div_ceil(in_b * (transform->grid_size-1), 255);
-		float rx = linear_r * (transform->grid_size-1) - x;
-		float ry = linear_g * (transform->grid_size-1) - y;
-		float rz = linear_b * (transform->grid_size-1) - z;
+		// xx = in_c * (transform->grid - 1 ), placed into 32-bit words
+		px = _mm_mullo_epi16(px, _mm_set1_epi16(transform->grid_size - 1));
 
-		c0_r = CLU(r_table, x, y, z);
-		c0_g = CLU(g_table, x, y, z);
-		c0_b = CLU(b_table, x, y, z);
+		// rr = (in_c * (transform->grid_size - 1)) / 255.0f 
+		rr = _mm_cvtepi32_ps(px);
+		rr = _mm_div_ps(px, _mm_set1_ps(255.0f));
 
-		if( rx >= ry ) {
-			if (ry >= rz) { //rx >= ry && ry >= rz
-				c1_r = CLU(r_table, x_n, y, z) - c0_r;
-				c2_r = CLU(r_table, x_n, y_n, z) - CLU(r_table, x_n, y, z);
-				c3_r = CLU(r_table, x_n, y_n, z_n) - CLU(r_table, x_n, y_n, z);
-				c1_g = CLU(g_table, x_n, y, z) - c0_g;
-				c2_g = CLU(g_table, x_n, y_n, z) - CLU(g_table, x_n, y, z);
-				c3_g = CLU(g_table, x_n, y_n, z_n) - CLU(g_table, x_n, y_n, z);
-				c1_b = CLU(b_table, x_n, y, z) - c0_b;
-				c2_b = CLU(b_table, x_n, y_n, z) - CLU(b_table, x_n, y, z);
-				c3_b = CLU(b_table, x_n, y_n, z_n) - CLU(b_table, x_n, y_n, z);
-			} else {
-				if (rx >= rz) { //rx >= rz && rz >= ry
-					c1_r = CLU(r_table, x_n, y, z) - c0_r;
-					c2_r = CLU(r_table, x_n, y_n, z_n) - CLU(r_table, x_n, y, z_n);
-					c3_r = CLU(r_table, x_n, y, z_n) - CLU(r_table, x_n, y, z);
-					c1_g = CLU(g_table, x_n, y, z) - c0_g;
-					c2_g = CLU(g_table, x_n, y_n, z_n) - CLU(g_table, x_n, y, z_n);
-					c3_g = CLU(g_table, x_n, y, z_n) - CLU(g_table, x_n, y, z);
-					c1_b = CLU(b_table, x_n, y, z) - c0_b;
-					c2_b = CLU(b_table, x_n, y_n, z_n) - CLU(b_table, x_n, y, z_n);
-					c3_b = CLU(b_table, x_n, y, z_n) - CLU(b_table, x_n, y, z);
-				} else { //rz > rx && rx >= ry
-					c1_r = CLU(r_table, x_n, y, z_n) - CLU(r_table, x, y, z_n);
-					c2_r = CLU(r_table, x_n, y_n, z_n) - CLU(r_table, x_n, y, z_n);
-					c3_r = CLU(r_table, x, y, z_n) - c0_r;
-					c1_g = CLU(g_table, x_n, y, z_n) - CLU(g_table, x, y, z_n);
-					c2_g = CLU(g_table, x_n, y_n, z_n) - CLU(g_table, x_n, y, z_n);
-					c3_g = CLU(g_table, x, y, z_n) - c0_g;
-					c1_b = CLU(b_table, x_n, y, z_n) - CLU(b_table, x, y, z_n);
-					c2_b = CLU(b_table, x_n, y_n, z_n) - CLU(b_table, x_n, y, z_n);
-					c3_b = CLU(b_table, x, y, z_n) - c0_b;
-				}
-			}
-		} else {
-			if (rx >= rz) { //ry > rx && rx >= rz
-				c1_r = CLU(r_table, x_n, y_n, z) - CLU(r_table, x, y_n, z);
-				c2_r = CLU(r_table, x, y_n, z) - c0_r;
-				c3_r = CLU(r_table, x_n, y_n, z_n) - CLU(r_table, x_n, y_n, z);
-				c1_g = CLU(g_table, x_n, y_n, z) - CLU(g_table, x, y_n, z);
-				c2_g = CLU(g_table, x, y_n, z) - c0_g;
-				c3_g = CLU(g_table, x_n, y_n, z_n) - CLU(g_table, x_n, y_n, z);
-				c1_b = CLU(b_table, x_n, y_n, z) - CLU(b_table, x, y_n, z);
-				c2_b = CLU(b_table, x, y_n, z) - c0_b;
-				c3_b = CLU(b_table, x_n, y_n, z_n) - CLU(b_table, x_n, y_n, z);
-			} else {
-				if (ry >= rz) { //ry >= rz && rz > rx
-					c1_r = CLU(r_table, x_n, y_n, z_n) - CLU(r_table, x, y_n, z_n);
-					c2_r = CLU(r_table, x, y_n, z) - c0_r;
-					c3_r = CLU(r_table, x, y_n, z_n) - CLU(r_table, x, y_n, z);
-					c1_g = CLU(g_table, x_n, y_n, z_n) - CLU(g_table, x, y_n, z_n);
-					c2_g = CLU(g_table, x, y_n, z) - c0_g;
-					c3_g = CLU(g_table, x, y_n, z_n) - CLU(g_table, x, y_n, z);
-					c1_b = CLU(b_table, x_n, y_n, z_n) - CLU(b_table, x, y_n, z_n);
-					c2_b = CLU(b_table, x, y_n, z) - c0_b;
-					c3_b = CLU(b_table, x, y_n, z_n) - CLU(b_table, x, y_n, z);
-				} else { //rz > ry && ry > rx
-					c1_r = CLU(r_table, x_n, y_n, z_n) - CLU(r_table, x, y_n, z_n);
-					c2_r = CLU(r_table, x, y_n, z_n) - CLU(r_table, x, y, z_n);
-					c3_r = CLU(r_table, x, y, z_n) - c0_r;
-					c1_g = CLU(g_table, x_n, y_n, z_n) - CLU(g_table, x, y_n, z_n);
-					c2_g = CLU(g_table, x, y_n, z_n) - CLU(g_table, x, y, z_n);
-					c3_g = CLU(g_table, x, y, z_n) - c0_g;
-					c1_b = CLU(b_table, x_n, y_n, z_n) - CLU(b_table, x, y_n, z_n);
-					c2_b = CLU(b_table, x, y_n, z_n) - CLU(b_table, x, y, z_n);
-					c3_b = CLU(b_table, x, y, z_n) - c0_b;
-				}
-			}
+		// Effectively compute the floor by truncating the float.
+		xx = _mm_cvttps_epi32(rr);
+		xn = _mm_cvttps_epi32(_mm_add_ps(rr, _mm_set1_ps(254.0f / 255.0f)));
+
+		// Calculate fractional component.
+		rr = _mm_sub_ps(rr, _mm_cvtepi32_ps(xx));
+
+		// Compare components.
+		rrcmp  = _mm_cmpge_ps(_mm_shuffle_ps(rr, rr, _MM_SHUFFLE(0, 0, 1, 0)), _mm_shuffle_ps(rr, rr, _MM_SHUFFLE(0, 2, 2, 1)));
+
+		// pack xyz back into 16-bit words
+		xx = _mm_packs_epi32(xx, xx);
+		xn = _mm_packs_epi32(xn, xn);
+
+		// assumes RGBA (r = lowest 16-bit, g next, b next, alpha next)
+		px = _mm_set_epi16(0, 0, 0, 0, 0, xy_len, x_len, len);
+
+		// x * len, y * x_len, z * xy_len
+		xx = _mm_mullo_epi16(xx, px);
+		xn = _mm_mullo_epi16(xn, px);
+		_mm_storeu_si128((__m128i*)&offset_l[0], xx);
+		_mm_storeu_si128((__m128i*)&offset_h[0], xn);
+
+		p0 = CLU_OFFSET(l, l, l);
+		p3 = CLU_OFFSET(h, h, h);
+		switch (_mm_movemask_ps(rrcmp)) {
+			case 8: // rx < ry && ry < rz && rx < rz
+				p1 = CLU_OFFSET(l, h, h);
+				p2 = CLU_OFFSET(l, l, h);
+				cr0 = _mm_set_ps(0.0f, r_table[p3], r_table[p1], r_table[p2]);
+				cr1 = _mm_set_ps(0.0f, r_table[p1], r_table[p2], r_table[p0]);
+				cg0 = _mm_set_ps(0.0f, g_table[p3], g_table[p1], g_table[p2]);
+				cg1 = _mm_set_ps(0.0f, g_table[p1], g_table[p2], g_table[p0]);
+				cb0 = _mm_set_ps(0.0f, b_table[p3], b_table[p1], b_table[p2]);
+				cb1 = _mm_set_ps(0.0f, b_table[p1], b_table[p2], b_table[p0]);
+				break;
+			case 10: // rx < ry && ry >= rz && rx < rz
+				p1 = CLU_OFFSET(l, h, h);
+				p2 = CLU_OFFSET(l, h, l);
+				cr0 = _mm_set_ps(0.0f, r_table[p3], r_table[p2], r_table[p1]);
+				cr1 = _mm_set_ps(0.0f, r_table[p1], r_table[p0], r_table[p2]);
+				cg0 = _mm_set_ps(0.0f, g_table[p3], g_table[p2], g_table[p1]);
+				cg1 = _mm_set_ps(0.0f, g_table[p1], g_table[p0], g_table[p2]);
+				cb0 = _mm_set_ps(0.0f, b_table[p3], b_table[p2], b_table[p1]);
+				cb1 = _mm_set_ps(0.0f, b_table[p1], b_table[p0], b_table[p2]);
+				break;
+			case 12: // rx < ry && ry < rz && rx >= rz
+			case 14: // rx < ry && ry >= rz && rx >= rz
+				p1 = CLU_OFFSET(h, h, l);
+				p2 = CLU_OFFSET(l, h, l);
+				cr0 = _mm_set_ps(0.0f, r_table[p1], r_table[p2], r_table[p3]);
+				cr1 = _mm_set_ps(0.0f, r_table[p2], r_table[p0], r_table[p1]);
+				cg0 = _mm_set_ps(0.0f, g_table[p1], g_table[p2], g_table[p3]);
+				cg1 = _mm_set_ps(0.0f, g_table[p2], g_table[p0], g_table[p1]);
+				cb0 = _mm_set_ps(0.0f, b_table[p1], b_table[p2], b_table[p3]);
+				cb1 = _mm_set_ps(0.0f, b_table[p2], b_table[p0], b_table[p1]);
+				break;
+			case 9: // rx >= ry && ry < rz && rx < rz
+				p1 = CLU_OFFSET(h, l, h);
+				p2 = CLU_OFFSET(l, l, h);
+				cr0 = _mm_set_ps(0.0f, r_table[p1], r_table[p3], r_table[p2]);
+				cr1 = _mm_set_ps(0.0f, r_table[p2], r_table[p1], r_table[p0]);
+				cg0 = _mm_set_ps(0.0f, g_table[p1], g_table[p3], g_table[p2]);
+				cg1 = _mm_set_ps(0.0f, g_table[p2], g_table[p1], g_table[p0]);
+				cb0 = _mm_set_ps(0.0f, b_table[p1], b_table[p3], b_table[p2]);
+				cb1 = _mm_set_ps(0.0f, b_table[p2], b_table[p1], b_table[p0]);
+				break;
+			case 13: // rx >= ry && ry < rz && rx >= rz
+				p1 = CLU_OFFSET(h, l, l);
+				p2 = CLU_OFFSET(h, l, h);
+				cr0 = _mm_set_ps(0.0f, r_table[p1], r_table[p3], r_table[p2]);
+				cr1 = _mm_set_ps(0.0f, r_table[p0], r_table[p2], r_table[p1]);
+				cg0 = _mm_set_ps(0.0f, g_table[p1], g_table[p3], g_table[p2]);
+				cg1 = _mm_set_ps(0.0f, g_table[p0], g_table[p2], g_table[p1]);
+				cb0 = _mm_set_ps(0.0f, b_table[p1], b_table[p3], b_table[p2]);
+				cb1 = _mm_set_ps(0.0f, b_table[p0], b_table[p2], b_table[p1]);
+				break;
+			default: // Can never happen
+			case 11: // rx >= ry && ry >= rz && rx < rz
+			case 15: // rx >= ry && ry >= rz && rx >= rz
+				p1 = CLU_OFFSET(h, l, l);
+				p2 = CLU_OFFSET(h, h, l);
+				cr0 = _mm_set_ps(0.0f, r_table[p1], r_table[p2], r_table[p3]);
+				cr1 = _mm_set_ps(0.0f, r_table[p0], r_table[p1], r_table[p2]);
+				cg0 = _mm_set_ps(0.0f, g_table[p1], g_table[p2], g_table[p3]);
+				cg0 = _mm_set_ps(0.0f, g_table[p1], g_table[p2], g_table[p3]);
+				cb1 = _mm_set_ps(0.0f, b_table[p0], b_table[p1], b_table[p2]);
+				cb1 = _mm_set_ps(0.0f, b_table[p0], b_table[p1], b_table[p2]);
+				break;
 		}
 
-		clut_r = c0_r + c1_r*rx + c2_r*ry + c3_r*rz;
-		clut_g = c0_g + c1_g*rx + c2_g*ry + c3_g*rz;
-		clut_b = c0_b + c1_b*rx + c2_b*ry + c3_b*rz;
+		// xx c0_r c0_g c0b
+		// xx c1_r 
+		// sum 
 
-		dest[kRIndex] = clamp_u8(clut_r*255.0f);
-		dest[kGIndex] = clamp_u8(clut_g*255.0f);
-		dest[kBIndex] = clamp_u8(clut_b*255.0f);
+		// highest register needs to be 1.0
+		rr = _mm_mul_ps(rr, _mm_set_ps(0.0, 1.0, 1.0, 1.0));
+		rr = _mm_add_ps(rr, _mm_set_ps(1.0, 0.0, 0.0, 0.0));
+
+		// clut_r = c0_r + c1_r*rx + c2_r*ry + c3_r*rz;
+		// clut_g = c0_g + c1_g*rx + c2_g*ry + c3_g*rz;
+		// clut_b = c0_b + c1_b*rx + c2_b*ry + c3_b*rz;
+		cr0 = _mm_sub_ps(cr0, cr1);
+		cg0 = _mm_sub_ps(cg0, cg1);
+		cb0 = _mm_sub_ps(cb0, cb1);
+
+		cr0 = _mm_mul_ps(cr0, rr);
+		cg0 = _mm_mul_ps(cg0, rr);
+		cb0 = _mm_mul_ps(cb0, rr);
+
+		// add 32-bits into lowest 32-bit register
+		cr0 = _mm_add_ps(cr0, _mm_movehl_ps(cr0, cr0));
+		cg0 = _mm_add_ps(cg0, _mm_movehl_ps(cg0, cg0));
+		cb0 = _mm_add_ps(cb0, _mm_movehl_ps(cb0, cb0));
+
+		cr0 = _mm_add_ss(cr0, _mm_shuffle_ps(cr0, cr0, _MM_SHUFFLE(0, 0, 0, 1)));
+		cg0 = _mm_add_ss(cg0, _mm_shuffle_ps(cg0, cg0, _MM_SHUFFLE(0, 0, 0, 1)));
+		cb0 = _mm_add_ss(cb0, _mm_shuffle_ps(cb0, cb0, _MM_SHUFFLE(0, 0, 0, 1)));
+
+		// combine components into single 128-bit register
+		rr = _mm_shuffle_ps(cr0, cg0, _MM_SHUFFLE(0, 0, 0, 0));
+		rr = _mm_shuffle_ps(rr, rr, _MM_SHUFFLE(0, 0, 2, 0));
+		rr = _mm_shuffle_ps(rr, cb0, _MM_SHUFFLE(0, 0, 1, 0));
+
+		// clamp components from 0 - 255
+		rr = _mm_mul_ss(rr, _mm_set1_ps(255.0f));
+		rr = _mm_add_ss(rr, _mm_set1_ps(0.5f));
+		rr = _mm_min_ss(rr, _mm_set1_ps(255.0f));
+		rr = _mm_max_ss(rr, _mm_set1_ps(0.0f));
+
+		// convert final result into RGB component values
+		xx = _mm_cvtps_epi32(rr);
+		_mm_storeu_si128((__m128i*)&offset_l[0], xx);
+
+		dest[kRIndex] = offset_l[0];
+		dest[kGIndex] = offset_l[1];
+		dest[kBIndex] = offset_l[2];
 		if (kAIndex != NO_A_INDEX) {
 			dest[kAIndex] = in_a;
 		}
+
+		src += components;
 		dest += components;
 	}
 }
@@ -289,7 +354,7 @@ void qcms_transform_data_tetra_clut_rgb_sse2(const qcms_transform *transform,
                                              unsigned char *dest,
                                              size_t length)
 {
-  qcms_transform_data_template_tetra_clut_sse2<RGBA_R_INDEX, RGBA_G_INDEX, RGBA_B_INDEX>(transform, src, dest, length);
+  qcms_transform_data_template_tetra_clut_sse2<false>(transform, src, dest, length);
 }
 
 void qcms_transform_data_tetra_clut_rgba_sse2(const qcms_transform *transform,
@@ -297,7 +362,7 @@ void qcms_transform_data_tetra_clut_rgba_sse2(const qcms_transform *transform,
                                               unsigned char *dest,
                                               size_t length)
 {
-  qcms_transform_data_template_tetra_clut_sse2<RGBA_R_INDEX, RGBA_G_INDEX, RGBA_B_INDEX, RGBA_A_INDEX>(transform, src, dest, length);
+  qcms_transform_data_template_tetra_clut_sse2<false, RGBA_A_INDEX>(transform, src, dest, length);
 }
 
 void qcms_transform_data_tetra_clut_bgra_sse2(const qcms_transform *transform,
@@ -305,5 +370,5 @@ void qcms_transform_data_tetra_clut_bgra_sse2(const qcms_transform *transform,
                                               unsigned char *dest,
                                               size_t length)
 {
-  qcms_transform_data_template_tetra_clut_sse2<BGRA_R_INDEX, BGRA_G_INDEX, BGRA_B_INDEX, BGRA_A_INDEX>(transform, src, dest, length);
+  qcms_transform_data_template_tetra_clut_sse2<true, BGRA_A_INDEX>(transform, src, dest, length);
 }
