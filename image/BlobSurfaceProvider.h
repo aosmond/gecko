6e514fbde6440bf0357af12a5cd33a3c792603d8
Andrew Osmond <aosmond@mozilla.com>
2021-09-23 11:42:37 -0400

    move blob from surface to provider

--- image/BlobSurfaceProvider.h
+++ image/BlobSurfaceProvider.h
@@ -12,6 +12,7 @@
 #include "mozilla/gfx/2D.h"
 #include "mozilla/layers/WebRenderLayerManager.h"
 #include "ImageRegion.h"
+#include "ISurfaceProvider.h"
 
 #include <vector>
 
@@ -75,40 +76,56 @@ class SVGDocumentWrapper;
  * each SourceSurfaceBlobImage can only be used by one WebRenderLayerManager
  * because the recording is tied to a particular instance.
  */
-class SourceSurfaceBlobImage final : public gfx::SourceSurface {
+class BlobSurfaceProvider final : public ISurfaceProvider {
  public:
-  MOZ_DECLARE_REFCOUNTED_VIRTUAL_TYPENAME(SourceSurfaceBlobImage, override)
+  NS_INLINE_DECL_THREADSAFE_REFCOUNTING(BlobSurfaceProvider, override)
 
-  SourceSurfaceBlobImage(SVGDocumentWrapper* aSVGDocumentWrapper,
-                         const Maybe<SVGImageContext>& aSVGContext,
-                         const Maybe<ImageIntRegion>& aRegion,
-                         const gfx::IntSize& aSize, uint32_t aWhichFrame,
-                         uint32_t aImageFlags);
+  BlobSurfaceProvider(ImageKey aImageKey, const SurfaceKey& aSurfaceKey,
+                      SVGDocumentWrapper* aSVGDocumentWrapper,
+                      const Maybe<SVGImageContext>& aSVGContext,
+                      const Maybe<ImageIntRegion>& aRegion,
+                      uint32_t aImageFlags);
 
-  Maybe<wr::BlobImageKey> UpdateKey(layers::WebRenderLayerManager* aManager,
-                                    wr::IpcResourceUpdateQueue& aResources);
+  bool IsFinished() const override { return true; }
 
-  void MarkDirty();
-
-  gfx::SurfaceType GetType() const override {
-    return gfx::SurfaceType::BLOB_IMAGE;
-  }
-  gfx::IntSize GetSize() const override { return mSize; }
-  gfx::SurfaceFormat GetFormat() const override {
-    return gfx::SurfaceFormat::OS_RGBA;
-  }
-  already_AddRefed<gfx::DataSourceSurface> GetDataSurface() override {
-    return nullptr;
+  size_t LogicalSizeInBytes() const override {
+    const gfx::IntSize& size = GetSurfaceKey().Size();
+    return size.width * size.height * sizeof(uint32_t);
   }
 
-  void SizeOfExcludingThis(MallocSizeOf aMallocSizeOf,
-                           SizeOfInfo& aInfo) const override {
-    aInfo.AddType(gfx::SurfaceType::BLOB_IMAGE);
-    aInfo.mHeapBytes += mKeys.ShallowSizeOfExcludingThis(aMallocSizeOf);
+  nsresult UpdateKey(layers::RenderRootStateManager* aManager,
+                     wr::IpcResourceUpdateQueue& aResources,
+                     wr::ImageKey& aKey) override;
+
+  void InvalidateRecording() override;
+
+  void AddSizeOfExcludingThis(MallocSizeOf aMallocSizeOf,
+                              const AddSizeOfCb& aCallback) override {
+    AddSizeOfCbData metadata;
+    metadata.mFinished = true;
+    metadata.mHeapBytes += mKeys.ShallowSizeOfExcludingThis(aMallocSizeOf);
+
+    SourceSurface::SizeOfInfo info;
+    info.AddType(gfx::SurfaceType::BLOB_IMAGE);
+    metadata.Accumulate(info);
+
+    aCallback(metadata);
   }
+
+  DrawableSurface Surface() override {
+    return DrawableSurface(WrapNotNull(this));
+  }
+
+ protected:
+  DrawableFrameRef DrawableRef(size_t aFrame) override {
+    MOZ_ASSERT_UNREACHABLE("BlobSurfaceProvider::DrawableRef not supported!");
+    return DrawableFrameRef();
+  }
+  bool IsLocked() const override { return true; }
+  void SetLocked(bool) override {}
 
  private:
-  ~SourceSurfaceBlobImage() override;
+  ~BlobSurfaceProvider() override;
 
   Maybe<BlobImageKeyData> RecordDrawing(layers::WebRenderLayerManager* aManager,
                                         wr::IpcResourceUpdateQueue& aResources,
@@ -121,8 +138,6 @@ class SourceSurfaceBlobImage final : public gfx::SourceSurface {
   RefPtr<image::SVGDocumentWrapper> mSVGDocumentWrapper;
   Maybe<SVGImageContext> mSVGContext;
   Maybe<ImageIntRegion> mRegion;
-  gfx::IntSize mSize;
-  uint32_t mWhichFrame;
   uint32_t mImageFlags;
 };
 
