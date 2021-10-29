d93573d49e6d6458d2cfe8e47cf97358dee96090
Andrew Osmond <aosmond@mozilla.com>
2021-10-28 23:11:44 -0400

    Bug 1736177 - Part 5. Refactor WebRender display list building integration.
    This patch separates out a class, nsICanvasRenderingDisplay, for use on
    the display list building pipeline. nsICanvasRenderingContextInternal
    inherits from this and all of the canvas contexts implement the APIs the
    same as today for display purposes. This will make it easier to provide
    a display only class for OffscreenCanvas.
    
    It also standardizes on SupportsWeakPtr/WeakPtr instead of mixing in C++
    standard library versions.

--- dom/canvas/CanvasRenderingContext2D.cpp
+++ dom/canvas/CanvasRenderingContext2D.cpp
@@ -839,6 +839,7 @@ NS_IMPL_CYCLE_COLLECTION_UNLINK_BEGIN(CanvasRenderingContext2D)
     }
   }
   NS_IMPL_CYCLE_COLLECTION_UNLINK_PRESERVED_WRAPPER
+  NS_IMPL_CYCLE_COLLECTION_UNLINK_WEAK_PTR
 NS_IMPL_CYCLE_COLLECTION_UNLINK_END
 
 NS_IMPL_CYCLE_COLLECTION_TRAVERSE_BEGIN(CanvasRenderingContext2D)
@@ -894,6 +895,7 @@ NS_IMPL_CYCLE_COLLECTION_CAN_SKIP_THIS_END
 
 NS_INTERFACE_MAP_BEGIN_CYCLE_COLLECTION(CanvasRenderingContext2D)
   NS_WRAPPERCACHE_INTERFACE_MAP_ENTRY
+  NS_INTERFACE_MAP_ENTRY(nsICanvasRenderingDisplay)
   NS_INTERFACE_MAP_ENTRY(nsICanvasRenderingContextInternal)
   NS_INTERFACE_MAP_ENTRY(nsISupports)
 NS_INTERFACE_MAP_END
@@ -5401,7 +5403,7 @@ bool CanvasRenderingContext2D::UpdateWebRenderCanvasData(
 
   if (!mResetLayer && renderer) {
     CanvasRendererData data;
-    data.mContext = mSharedPtrPtr;
+    data.mDisplay = this;
     data.mSize = GetSize();
 
     if (renderer->IsDataValid(data)) {
@@ -5424,7 +5426,7 @@ bool CanvasRenderingContext2D::UpdateWebRenderCanvasData(
 bool CanvasRenderingContext2D::InitializeCanvasRenderer(
     nsDisplayListBuilder* aBuilder, CanvasRenderer* aRenderer) {
   CanvasRendererData data;
-  data.mContext = mSharedPtrPtr;
+  data.mDisplay = this;
   data.mSize = GetSize();
   data.mIsOpaque = mOpaque;
   data.mDoPaintCallbacks = true;
