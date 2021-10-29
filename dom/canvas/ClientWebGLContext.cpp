fe16be6f093db3af0a91213eb0c5965e8bd5ec21
Andrew Osmond <aosmond@mozilla.com>
2021-10-28 23:11:44 -0400

    Bug 1736177 - Part 6. Refactor WebRender display list building integration.
    This patch separates out a class, nsICanvasRenderingDisplay, for use on
    the display list building pipeline. nsICanvasRenderingContextInternal
    inherits from this and all of the canvas contexts implement the APIs the
    same as today for display purposes. This will make it easier to provide
    a display only class for OffscreenCanvas.
    
    It also standardizes on SupportsWeakPtr/WeakPtr instead of mixing in C++
    standard library versions.
    
    Differential Revision: https://phabricator.services.mozilla.com/D130782

--- dom/canvas/ClientWebGLContext.cpp
+++ dom/canvas/ClientWebGLContext.cpp
@@ -384,6 +384,12 @@ Maybe<layers::SurfaceDescriptor> ClientWebGLContext::GetFrontBuffer(
   return ret;
 }
 
+Maybe<layers::SurfaceDescriptor> ClientWebGLContext::PresentFrontBuffer(
+    WebGLFramebufferJS* const fb, const layers::TextureType type, bool webvr) {
+  Present(fb, type, webvr);
+  return GetFrontBuffer(fb, webvr);
+}
+
 void ClientWebGLContext::ClearVRSwapChain() { Run<RPROC(ClearVRSwapChain)>(); }
 
 // -
@@ -414,7 +420,7 @@ bool ClientWebGLContext::InitializeCanvasRenderer(
   if (IsContextLost()) return false;
 
   layers::CanvasRendererData data;
-  data.mContext = mSharedPtrPtr;
+  data.mDisplay = this;
   data.mOriginPos = gl::OriginPos::BottomLeft;
 
   const auto& options = *mInitialOptions;
@@ -6378,6 +6384,7 @@ NS_IMPL_CYCLE_COLLECTION_WRAPPERCACHE(WebGLVertexArrayJS, mIndexBuffer,
 
 NS_INTERFACE_MAP_BEGIN_CYCLE_COLLECTION(ClientWebGLContext)
   NS_WRAPPERCACHE_INTERFACE_MAP_ENTRY
+  NS_INTERFACE_MAP_ENTRY(nsICanvasRenderingDisplay)
   NS_INTERFACE_MAP_ENTRY(nsICanvasRenderingContextInternal)
   NS_INTERFACE_MAP_ENTRY(nsISupports)
 NS_INTERFACE_MAP_END
