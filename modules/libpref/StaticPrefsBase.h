77a76241de1a3ab5f1e1207ea2be3bfe5c599f28
Andrew Osmond <aosmond@mozilla.com>
2022-08-11 12:53:05 -0400

    ignore default?

--- modules/libpref/StaticPrefsBase.h
+++ modules/libpref/StaticPrefsBase.h
@@ -23,8 +23,8 @@ typedef const char* String;
 
 class DataMutexString : public StaticDataMutex<nsCString> {
  public:
-  explicit DataMutexString(const nsLiteralCString& aDefault)
-      : StaticDataMutex<nsCString>(nsCString(aDefault), "") {}
+  explicit DataMutexString(const nsLiteralCString&)
+      : StaticDataMutex<nsCString>(nsCString(), "") {}
 };
 
 template <typename T>
