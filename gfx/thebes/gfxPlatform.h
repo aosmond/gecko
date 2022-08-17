e1e04d4e069eb5032f176e2bdfec9b1ab2001135
Andrew Osmond <aosmond@mozilla.com>
2022-08-17 17:10:35 -0400

    Bug 1785366 - Blocklist backdrop filter on some Ivy/Sandybridge Intel Windows drivers. r=jrmuizel,emilio
    Backdrop filter crashes newer Intel drivers on Windows. This patch adds
    support to the blocklist infrastructure for backdrop filter, and hooks
    this up with the CSS property table.
    
    Differential Revision: https://phabricator.services.mozilla.com/D154950

--- gfx/thebes/gfxPlatform.h
+++ gfx/thebes/gfxPlatform.h
@@ -824,6 +824,7 @@ class gfxPlatform : public mozilla::layers::MemoryPressureListener {
   virtual void InitWebGLConfig();
   virtual void InitWebGPUConfig();
   virtual void InitWindowOcclusionConfig();
+  void InitBackdropFilterConfig();
 
   virtual void GetPlatformDisplayInfo(mozilla::widget::InfoObject& aObj) {}
 
