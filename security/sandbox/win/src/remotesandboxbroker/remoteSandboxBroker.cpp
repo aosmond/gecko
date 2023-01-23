dfd2ae4b05c0f366dce8b386f02785187711ef67
Andrew Osmond <aosmond@mozilla.com>
2023-01-23 12:23:27 -0500

    fix launching broker with wrong arch path

--- security/sandbox/win/src/remotesandboxbroker/remoteSandboxBroker.cpp
+++ security/sandbox/win/src/remotesandboxbroker/remoteSandboxBroker.cpp
@@ -88,7 +88,7 @@ bool RemoteSandboxBroker::LaunchApp(
   // TaskQueue) to resolve our promise as it will be blocked until we return
   // from this function.
   nsCOMPtr<nsISerialEventTarget> target = NS_GetCurrentThread();
-  mParent.Launch(mParameters.shareHandles(), target)
+  mParent.Launch(mLaunchArch, mParameters.shareHandles(), target)
       ->Then(target, __func__, std::move(resolve), std::move(reject));
 
   // Spin the event loop while the sandbox launcher process launches.
@@ -164,10 +164,6 @@ bool RemoteSandboxBroker::SetSecurityLevelForUtilityProcess(
     mozilla::ipc::SandboxingKind aSandbox) {
   MOZ_CRASH(
       "RemoteSandboxBroker::SetSecurityLevelForUtilityProcess not Implemented");
-}
-
-AbstractSandboxBroker* CreateRemoteSandboxBroker() {
-  return new RemoteSandboxBroker(base::PROCESS_ARCH_INVALID);
 }
 
 }  // namespace mozilla
