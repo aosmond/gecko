5f79fd6ff8726d47d1dd320017da1897b1fd4756
Andrew Osmond <aosmond@mozilla.com>
2023-05-02 17:34:21 -0400

    WIP

---
+++ dom/media/platforms/agnostic/gmp/GMPEncoderModule.cpp
@@ -0,0 +1,34 @@
+/* -*- Mode: C++; tab-width: 2; indent-tabs-mode: nil; c-basic-offset: 2 -*- */
+/* vim:set ts=2 sw=2 sts=2 et cindent: */
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+#include "GMPEncoderModule.h"
+
+#include "GMPUtils.h"
+#include "GMPVideoEncoder.h"
+#include "MP4Decoder.h"
+
+namespace mozilla {
+
+bool GMPEncoderModule::SupportsMimeType(const nsACString& aMimeType) const {
+  if (!MP4Decoder::IsH264(aMimeType)) {
+    return false;
+  }
+
+  return HaveGMPFor("encode-video"_ns, {"h264"_ns});
+}
+
+already_AddRefed<MediaDataEncoder> GMPEncoderModule::CreateVideoEncoder(
+    const CreateEncoderParams& aParams) const {
+  if (!MP4Decoder::IsH264(aParams.mConfig.mMimeType)) {
+    return nullptr;
+  }
+
+  RefPtr<MediaDataEncoder> encoder(
+      new GMPVideoEncoder(aParams.ToH264Config(), aParams.mTaskQueue));
+  return encoder.forget();
+}
+
+}  // namespace mozilla
