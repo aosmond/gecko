b7a092fdc382494c11c085baccadec4e06a9c715
Andrew Osmond <aosmond@mozilla.com>
2023-07-17 14:02:42 -0400

    add imagedecoder construct validation

--- dom/media/webcodecs/ImageDecoder.cpp
+++ dom/media/webcodecs/ImageDecoder.cpp
@@ -5,6 +5,8 @@
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
 #include "mozilla/dom/ImageDecoder.h"
+#include "nsTHashSet.h"
+#include "mozilla/dom/ReadableStream.h"
 
 namespace mozilla::dom {
 
@@ -30,6 +32,84 @@ JSObject* ImageDecoder::WrapObject(JSContext* aCx,
 /* static */ already_AddRefed<ImageDecoder> ImageDecoder::Constructor(
     const GlobalObject& aGlobal, const ImageDecoderInit& aInit,
     ErrorResult& aRv) {
+  // 10.2.2.1 If init is not valid ImageDecoderInit, throw a TypeError.
+  // 10.3.1 If type is not a valid image MIME type, return false.
+  const auto mimeType = Substring(aInit.mType, 6);
+  if (!mimeType.Equals(u"image/"_ns)) {
+    aRv.ThrowTypeError("Invalid MIME type, must be 'image'");
+    return nullptr;
+  }
+
+  const uint8_t* dataContents = nullptr;
+  size_t dataSize = 0;
+
+  if (aInit.mData.IsReadableStream()) {
+    const auto& stream = aInit.mData.GetAsReadableStream();
+    // 10.3.2 If data is of type ReadableStream and the ReadableStream is
+    // disturbed or locked, return false.
+    if (stream->Disturbed() || stream->Locked()) {
+      aRv.ThrowTypeError("ReadableStream data is disturbed and/or locked");
+      return nullptr;
+    }
+  } else {
+    if (aInit.mData.IsArrayBufferView()) {
+      const auto& view = aInit.mData.GetAsArrayBufferView();
+      view.ComputeState();
+      dataContents = view.Data();
+      dataSize = view.Length();
+    } else if (aInit.mData.IsArrayBuffer()) {
+      const auto& buffer = aInit.mData.GetAsArrayBuffer();
+      buffer.ComputeState();
+      dataContents = buffer.Data();
+      dataSize = buffer.Length();
+    } else {
+      MOZ_ASSERT_UNREACHABLE("Unsupported data type!");
+      aRv.ThrowNotSupportedError("Unsupported data type");
+      return nullptr;
+    }
+
+    // 10.3.3.1 If the result of running IsDetachedBuffer (described in
+    // [ECMASCRIPT]) on data is false, return false.
+    if (!dataContents) {
+      aRv.ThrowTypeError("BufferSource is detached");
+      return nullptr;
+    }
+    // 10.3.3.2 If data is empty, return false.
+    if (dataSize == 0) {
+      aRv.ThrowTypeError("BufferSource is empty");
+      return nullptr;
+    }
+  }
+
+  // 10.3.4 If desiredWidth exists and desiredHeight does not exist, return
+  // false. 10.3.5 If desiredHeight exists and desiredWidth does not exist,
+  // return false.
+  if (aInit.mDesiredHeight.WasPassed() != aInit.mDesiredWidth.WasPassed()) {
+    aRv.ThrowTypeError(
+        "Both or neither of desiredHeight and desiredWidth must be passed");
+    return nullptr;
+  }
+
+  nsTHashSet<const uint8_t*> transferSet;
+  for (const auto& buffer : aInit.mTransfer) {
+    buffer.ComputeState();
+    const uint8_t* transferDataContents = buffer.Data();
+    // 10.2.2.3.1 If [[Detached]] internal slot is true, then throw a
+    // DataCloneError DOMException.
+    if (!transferDataContents) {
+      aRv.ThrowDataCloneError("Transfer contains detached ArrayBuffer objects");
+      return nullptr;
+    }
+    // 10.2.2.2 If init.transfer contains more than one reference to the same
+    // ArrayBuffer, then throw a DataCloneError DOMException.
+    if (transferSet.Contains(transferDataContents)) {
+      aRv.ThrowDataCloneError(
+          "Transfer contains duplicate ArrayBuffer objects");
+      return nullptr;
+    }
+    transferSet.Insert(transferDataContents);
+  }
+
   return nullptr;
 }
 
