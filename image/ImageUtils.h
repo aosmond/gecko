b91c7ec07cd3ada4270da14015fa72fe555e89cf
Andrew Osmond <aosmond@mozilla.com>
2023-07-25 19:31:16 -0400

    p1 imagelib prep

---
+++ image/ImageUtils.h
@@ -0,0 +1,116 @@
+/* -*- Mode: C++; tab-width: 2; indent-tabs-mode: nil; c-basic-offset: 2 -*- */
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+#ifndef mozilla_image_ImageUtils_h
+#define mozilla_image_ImageUtils_h
+
+#include "FrameTimeout.h"
+#include "mozilla/image/SurfaceFlags.h"
+#include "mozilla/Assertions.h"
+#include "mozilla/MozPromise.h"
+#include "mozilla/RefPtr.h"
+#include "nsString.h"
+#include "nsTArray.h"
+
+namespace mozilla {
+class ErrorResult;
+
+namespace gfx {
+class SourceSurface;
+}
+
+namespace image {
+class SourceBuffer;
+
+/**
+ * The type of decoder; this is usually determined from a MIME type using
+ * DecoderFactory::GetDecoderType() or ImageUtils::GetDecoderType().
+ */
+enum class DecoderType {
+  PNG,
+  GIF,
+  JPEG,
+  BMP,
+  BMP_CLIPBOARD,
+  ICO,
+  ICON,
+  WEBP,
+  AVIF,
+  JXL,
+  UNKNOWN
+};
+
+struct DecodeMetadataResult {
+  int32_t mWidth = 0;
+  int32_t mHeight = 0;
+  int32_t mRepetitions = -1;
+  bool mAnimated = false;
+};
+
+struct DecodeFrameCountResult {
+  uint32_t mFrameCount = 0;
+  bool mFinished = false;
+};
+
+struct DecodedFrame {
+  RefPtr<gfx::SourceSurface> mSurface;
+  FrameTimeout mTimeout;
+};
+
+struct DecodeFramesResult {
+  nsTArray<DecodedFrame> mFrames;
+  bool mFinished = false;
+};
+
+using DecodeMetadataPromise = MozPromise<DecodeMetadataResult, nsresult, true>;
+using DecodeFrameCountPromise =
+    MozPromise<DecodeFrameCountResult, nsresult, true>;
+using DecodeFramesPromise = MozPromise<DecodeFramesResult, nsresult, true>;
+
+class AnonymousDecoder {
+ public:
+  NS_INLINE_DECL_PURE_VIRTUAL_REFCOUNTING
+
+  virtual RefPtr<DecodeMetadataPromise> Initialize() = 0;
+
+  virtual void Destroy() = 0;
+
+  virtual RefPtr<DecodeFrameCountPromise> DecodeFrameCount(
+      uint32_t aKnownFrameCount) = 0;
+
+  virtual RefPtr<DecodeFramesPromise> DecodeFrames(size_t aCount) = 0;
+
+  virtual void CancelDecodeFrames() = 0;
+
+ protected:
+  AnonymousDecoder();
+  virtual ~AnonymousDecoder();
+};
+
+using CreateBufferPromise = MozPromise<RefPtr<SourceBuffer>, nsresult, true>;
+
+class ImageUtils {
+ public:
+  static RefPtr<CreateBufferPromise> CreateSourceBuffer(
+      nsIInputStream* aStream);
+
+  static RefPtr<CreateBufferPromise> CreateSourceBuffer(const uint8_t* aData,
+                                                        size_t aLength);
+
+  static already_AddRefed<AnonymousDecoder> CreateDecoder(
+      SourceBuffer* aSourceBuffer, DecoderType aType,
+      SurfaceFlags aSurfaceFlags);
+
+  static DecoderType GetDecoderType(const nsACString& aMimeType);
+
+ private:
+  ImageUtils() = delete;
+  ~ImageUtils() = delete;
+};
+
+}  // namespace image
+}  // namespace mozilla
+
+#endif  // mozilla_image_ImageUtils_h
