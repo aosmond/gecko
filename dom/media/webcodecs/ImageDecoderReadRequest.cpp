cbe700607be6452329c822d15e4bcdba235f1eb4
Andrew Osmond <aosmond@mozilla.com>
2023-07-25 19:32:59 -0400

    p2 webcodecs

---
+++ dom/media/webcodecs/ImageDecoderReadRequest.cpp
@@ -0,0 +1,162 @@
+/* -*- Mode: C++; tab-width: 2; indent-tabs-mode: nil; c-basic-offset: 2 -*- */
+/* vim:set ts=2 sw=2 sts=2 et cindent: */
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+#include "ImageDecoderReadRequest.h"
+#include "mozilla/CycleCollectedJSContext.h"
+#include "mozilla/dom/ImageDecoder.h"
+#include "mozilla/dom/ReadableStreamDefaultReader.h"
+#include "mozilla/image/SourceBuffer.h"
+
+namespace mozilla::dom {
+
+NS_IMPL_CYCLE_COLLECTION_INHERITED(ImageDecoderReadRequest, ReadRequest,
+                                   mDecoder, mReader)
+NS_IMPL_ADDREF_INHERITED(ImageDecoderReadRequest, ReadRequest)
+NS_IMPL_RELEASE_INHERITED(ImageDecoderReadRequest, ReadRequest)
+NS_INTERFACE_MAP_BEGIN_CYCLE_COLLECTION(ImageDecoderReadRequest)
+NS_INTERFACE_MAP_END_INHERITING(ReadRequest)
+
+ImageDecoderReadRequest::ImageDecoderReadRequest(
+    image::SourceBuffer* aSourceBuffer)
+    : mSourceBuffer(std::move(aSourceBuffer)) {}
+
+ImageDecoderReadRequest::~ImageDecoderReadRequest() = default;
+
+bool ImageDecoderReadRequest::Initialize(const GlobalObject& aGlobal,
+                                         ImageDecoder* aDecoder,
+                                         ReadableStream& aStream) {
+  IgnoredErrorResult rv;
+  mReader = ReadableStreamDefaultReader::Constructor(aGlobal, aStream, rv);
+  if (NS_WARN_IF(rv.Failed())) {
+    mSourceBuffer->Complete(NS_ERROR_FAILURE);
+    return false;
+  }
+
+  mDecoder = aDecoder;
+  QueueRead();
+  return true;
+}
+
+void ImageDecoderReadRequest::Destroy() {
+  mDecoder = nullptr;
+  mReader = nullptr;
+
+  if (mSourceBuffer) {
+    if (!mSourceBuffer->IsComplete()) {
+      mSourceBuffer->Complete(NS_ERROR_ABORT);
+    }
+    mSourceBuffer = nullptr;
+  }
+}
+
+void ImageDecoderReadRequest::QueueRead() {
+  class ReadRunnable final : public CancelableRunnable {
+   public:
+    explicit ReadRunnable(ImageDecoderReadRequest* aOwner)
+        : CancelableRunnable(
+              "mozilla::dom::ImageDecoderReadRequest::QueueRead"),
+          mOwner(aOwner) {}
+
+    NS_IMETHODIMP Run() override {
+      mOwner->Read();
+      mOwner = nullptr;
+      return NS_OK;
+    }
+
+    nsresult Cancel() override {
+      mOwner->Complete(NS_ERROR_ABORT);
+      mOwner = nullptr;
+      return NS_OK;
+    }
+
+   private:
+    virtual ~ReadRunnable() {
+      if (mOwner) {
+        Cancel();
+      }
+    }
+
+    RefPtr<ImageDecoderReadRequest> mOwner;
+  };
+
+  if (!mReader) {
+    return;
+  }
+
+  CycleCollectedJSContext* context = CycleCollectedJSContext::Get();
+  if (NS_WARN_IF(!context)) {
+    Complete(NS_ERROR_FAILURE);
+    return;
+  }
+
+  auto task = MakeRefPtr<ReadRunnable>(this);
+  NS_DispatchToCurrentThread(task.forget());
+}
+
+void ImageDecoderReadRequest::Read() {
+  if (!mReader) {
+    return;
+  }
+
+  CycleCollectedJSContext* context = CycleCollectedJSContext::Get();
+  if (NS_WARN_IF(!context)) {
+    Complete(NS_ERROR_FAILURE);
+    return;
+  }
+
+  RefPtr<ImageDecoderReadRequest> self(this);
+  RefPtr<ReadableStreamDefaultReader> reader(mReader);
+
+  IgnoredErrorResult err;
+  JSContext* cx = context->Context();
+  reader->ReadChunk(cx, *self, err);
+  if (NS_WARN_IF(err.Failed())) {
+    Complete(NS_ERROR_FAILURE);
+  }
+}
+
+void ImageDecoderReadRequest::Complete(nsresult aErr) {
+  if (mSourceBuffer && !mSourceBuffer->IsComplete()) {
+    mSourceBuffer->Complete(aErr);
+  }
+
+  if (mDecoder) {
+    mDecoder->OnSourceBufferComplete(aErr);
+    mDecoder = nullptr;
+  }
+}
+
+void ImageDecoderReadRequest::ChunkSteps(JSContext* aCx,
+                                         JS::Handle<JS::Value> aChunk,
+                                         ErrorResult& aRv) {
+  RootedSpiderMonkeyInterface<Uint8Array> chunk(aCx);
+  if (!aChunk.isObject() || !chunk.Init(&aChunk.toObject())) {
+    Complete(NS_ERROR_FAILURE);
+    return;
+  }
+  chunk.ComputeState();
+
+  nsresult rv = mSourceBuffer->Append(
+      reinterpret_cast<const char*>(chunk.Data()), chunk.Length());
+  if (NS_WARN_IF(NS_FAILED(rv))) {
+    Complete(NS_ERROR_FAILURE);
+    return;
+  }
+
+  QueueRead();
+}
+
+void ImageDecoderReadRequest::CloseSteps(JSContext* aCx, ErrorResult& aRv) {
+  Complete(NS_OK);
+}
+
+void ImageDecoderReadRequest::ErrorSteps(JSContext* aCx,
+                                         JS::Handle<JS::Value> aError,
+                                         ErrorResult& aRv) {
+  Complete(NS_ERROR_FAILURE);
+}
+
+}  // namespace mozilla::dom
---
+++ dom/media/webcodecs/ImageTrackList.cpp
@@ -0,0 +1,105 @@
+/* -*- Mode: C++; tab-width: 2; indent-tabs-mode: nil; c-basic-offset: 2 -*- */
+/* vim:set ts=2 sw=2 sts=2 et cindent: */
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+#include "mozilla/dom/ImageTrackList.h"
+#include "mozilla/dom/ImageTrack.h"
+#include "mozilla/image/ImageUtils.h"
+
+namespace mozilla::dom {
+
+NS_IMPL_CYCLE_COLLECTION_WRAPPERCACHE(ImageTrackList, mParent, mReadyPromise,
+                                      mTracks)
+NS_IMPL_CYCLE_COLLECTING_ADDREF(ImageTrackList)
+NS_IMPL_CYCLE_COLLECTING_RELEASE(ImageTrackList)
+NS_INTERFACE_MAP_BEGIN_CYCLE_COLLECTION(ImageTrackList)
+  NS_WRAPPERCACHE_INTERFACE_MAP_ENTRY
+  NS_INTERFACE_MAP_ENTRY(nsISupports)
+NS_INTERFACE_MAP_END
+
+ImageTrackList::ImageTrackList(nsIGlobalObject* aParent) : mParent(aParent) {}
+
+ImageTrackList::~ImageTrackList() = default;
+
+JSObject* ImageTrackList::WrapObject(JSContext* aCx,
+                                     JS::Handle<JSObject*> aGivenProto) {
+  AssertIsOnOwningThread();
+  return ImageTrackList_Binding::Wrap(aCx, this, aGivenProto);
+}
+
+void ImageTrackList::Initialize(ErrorResult& aRv) {
+  mReadyPromise = Promise::Create(mParent, aRv);
+  if (NS_WARN_IF(aRv.Failed())) {
+    return;
+  }
+}
+
+void ImageTrackList::Destroy() {
+  mSelectedIndex = -1;
+  mTracks.Clear();
+}
+
+void ImageTrackList::MaybeResolveReady() {
+  if (mReadyPromise->State() != Promise::PromiseState::Pending) {
+    mReadyPromise = Promise::CreateInfallible(mParent);
+  }
+  mReadyPromise->MaybeResolveWithUndefined();
+  mIsReady = true;
+}
+
+void ImageTrackList::MaybeRejectReady(const nsACString& aReason) {
+  if (mReadyPromise->State() != Promise::PromiseState::Pending) {
+    mReadyPromise = Promise::CreateInfallible(mParent);
+  }
+  mReadyPromise->MaybeRejectWithInvalidStateError(aReason);
+  mIsReady = true;
+}
+
+void ImageTrackList::OnMetadataSuccess(
+    const image::DecodeMetadataResult& aMetadata) {
+  auto track = MakeRefPtr<ImageTrack>(
+      this, /* aIndex */ 0, /* aSelected */ true, aMetadata.mAnimated,
+      /* aFrameCount */ 0, aMetadata.mRepetitions);
+  mTracks.AppendElement(std::move(track));
+  mSelectedIndex = 0;
+  MaybeResolveReady();
+}
+
+void ImageTrackList::OnMetadataFailed(nsresult aErr) {
+  MaybeRejectReady("Metadata decoding failed"_ns);
+}
+
+void ImageTrackList::OnFrameCountSuccess(
+    const image::DecodeFrameCountResult& aResult) {
+  if (mTracks.IsEmpty()) {
+    return;
+  }
+
+  mTracks.LastElement()->OnFrameCountSuccess(aResult);
+}
+
+void ImageTrackList::SetSelectedIndex(int32_t aIndex, bool aSelected) {
+  MOZ_ASSERT(aIndex >= 0);
+  MOZ_ASSERT(uint32_t(aIndex) < mTracks.Length());
+
+  if (aSelected) {
+    if (mSelectedIndex == -1) {
+      mTracks[aIndex]->MarkSelected();
+      mSelectedIndex = aIndex;
+      MaybeResolveReady();
+    } else if (mSelectedIndex != aIndex) {
+      mTracks[mSelectedIndex]->ClearSelected();
+      mTracks[aIndex]->MarkSelected();
+      mSelectedIndex = aIndex;
+      MaybeResolveReady();
+    }
+  } else if (mSelectedIndex == aIndex) {
+    mTracks[mSelectedIndex]->ClearSelected();
+    mSelectedIndex = -1;
+    MaybeRejectReady("No track selected"_ns);
+  }
+}
+
+}  // namespace mozilla::dom
