fb42213926a6104f1256d7104dd42de23d37eb4b
Andrew Osmond <aosmond@mozilla.com>
2023-07-25 19:31:16 -0400

    Bug XXX - Implement promise based anonymous image decoder.

--- image/SourceBuffer.cpp
+++ image/SourceBuffer.cpp
@@ -295,23 +295,33 @@ void SourceBuffer::AddWaitingConsumer(IResumable* aConsumer) {
 
   MOZ_ASSERT(!mStatus, "Waiting when we're complete?");
 
-  if (aConsumer) {
+  if (aConsumer && !mWaitingConsumers.Contains(aConsumer)) {
     mWaitingConsumers.AppendElement(aConsumer);
   }
 }
 
-void SourceBuffer::ResumeWaitingConsumers() {
+void SourceBuffer::TakeWaitingConsumers(
+    nsTArray<RefPtr<IResumable>>& aWaitingConsumers) {
   mMutex.AssertCurrentThreadOwns();
 
-  if (mWaitingConsumers.Length() == 0) {
+  aWaitingConsumers = std::move(mWaitingConsumers);
+}
+
+void SourceBuffer::ResumeWaitingConsumers(
+    nsTArray<RefPtr<IResumable>>& aWaitingConsumers) {
+  mMutex.AssertNotCurrentThreadOwns();
+
+  if (AppShutdown::IsInOrBeyond(ShutdownPhase::XPCOMShutdownFinal)) {
+    // The DecodePool has already gone away.
+    aWaitingConsumers.Clear();
     return;
   }
 
-  for (uint32_t i = 0; i < mWaitingConsumers.Length(); ++i) {
-    mWaitingConsumers[i]->Resume();
+  for (uint32_t i = 0; i < aWaitingConsumers.Length(); ++i) {
+    aWaitingConsumers[i]->Resume();
   }
 
-  mWaitingConsumers.Clear();
+  aWaitingConsumers.Clear();
 }
 
 nsresult SourceBuffer::ExpectLength(size_t aExpectedLength) {
@@ -412,6 +422,7 @@ nsresult SourceBuffer::Append(const char* aData, size_t aLength) {
   }
 
   // Update shared data structures.
+  nsTArray<RefPtr<IResumable>> consumers;
   {
     MutexAutoLock lock(mMutex);
 
@@ -434,9 +445,11 @@ nsresult SourceBuffer::Append(const char* aData, size_t aLength) {
       }
     }
 
-    // Resume any waiting readers now that there's new data.
-    ResumeWaitingConsumers();
+    TakeWaitingConsumers(consumers);
   }
+
+  // Resume any waiting readers now that there's new data.
+  ResumeWaitingConsumers(consumers);
 
   return NS_OK;
 }
@@ -511,8 +524,15 @@ void SourceBuffer::Complete(nsresult aStatus) {
 
   mStatus = Some(aStatus);
 
-  // Resume any waiting consumers now that we're complete.
-  ResumeWaitingConsumers();
+  // Take any waiting consumers now that we're complete.
+  nsTArray<RefPtr<IResumable>> consumers;
+  TakeWaitingConsumers(consumers);
+
+  // Resume said consumers if we have any.
+  if (!consumers.IsEmpty()) {
+    MutexAutoUnlock lock(mMutex);
+    ResumeWaitingConsumers(consumers);
+  }
 
   // If we still have active consumers, just return.
   if (mConsumerCount > 0) {
