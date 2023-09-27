054ebb96db5144ed60b995e8750bfc359f02e487
Andrew Osmond <aosmond@mozilla.com>
2023-09-27 14:24:15 -0400

    Bug 1855742 - Part 10. Allow recording surfaces and textures to be created on DOM workers.
    Differential Revision: https://phabricator.services.mozilla.com/D189531

--- gfx/layers/client/TextureRecorded.h
+++ gfx/layers/client/TextureRecorded.h
@@ -8,17 +8,20 @@
 #define mozilla_layers_TextureRecorded_h
 
 #include "TextureClient.h"
-#include "mozilla/layers/CanvasChild.h"
+#include "mozilla/Mutex.h"
 #include "mozilla/layers/LayersTypes.h"
 
-namespace mozilla {
-namespace layers {
+namespace mozilla::layers {
+class CanvasChild;
+class CanvasDrawEventRecorder;
 
 class RecordedTextureData final : public TextureData {
  public:
-  RecordedTextureData(already_AddRefed<CanvasChild> aCanvasChild,
-                      gfx::IntSize aSize, gfx::SurfaceFormat aFormat,
-                      TextureType aTextureType);
+  RecordedTextureData(gfx::IntSize aSize, gfx::SurfaceFormat aFormat);
+
+  bool Init(TextureType aTextureType);
+
+  void DestroyOnOwningThread();
 
   void FillInfo(TextureData::Info& aInfo) const final;
 
@@ -31,6 +34,10 @@ class RecordedTextureData final : public TextureData {
   void EndDraw() final;
 
   already_AddRefed<gfx::SourceSurface> BorrowSnapshot() final;
+
+  void Forget(LayersIPCChannel* aAllocator) final {
+    Deallocate(aAllocator);
+  }
 
   void Deallocate(LayersIPCChannel* aAllocator) final;
 
@@ -46,23 +53,29 @@ class RecordedTextureData final : public TextureData {
   bool RequiresRefresh() const final;
 
  private:
+  friend class TextureData;
+
   DISALLOW_COPY_AND_ASSIGN(RecordedTextureData);
 
   ~RecordedTextureData() override;
 
+  void DestroyOnOwningThreadLocked() MOZ_REQUIRES(mMutex);
+
   int64_t mTextureId;
+
+  Mutex mMutex;
   RefPtr<CanvasChild> mCanvasChild;
-  gfx::IntSize mSize;
-  gfx::SurfaceFormat mFormat;
+  RefPtr<CanvasDrawEventRecorder> mRecorder MOZ_GUARDED_BY(mMutex);
   RefPtr<gfx::DrawTarget> mDT;
   RefPtr<gfx::SourceSurface> mSnapshot;
   ThreadSafeWeakPtr<gfx::SourceSurface> mSnapshotWrapper;
+  gfx::IntSize mSize;
+  gfx::SurfaceFormat mFormat;
   OpenMode mLockedMode;
   layers::RemoteTextureId mLastRemoteTextureId;
   layers::RemoteTextureOwnerId mRemoteTextureOwnerId;
 };
 
-}  // namespace layers
-}  // namespace mozilla
+}  // namespace mozilla::layers
 
 #endif  // mozilla_layers_TextureRecorded_h
