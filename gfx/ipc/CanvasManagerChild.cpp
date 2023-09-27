3de18c21410ff65a763a95c875db813207fd80d8
Andrew Osmond <aosmond@mozilla.com>
2023-09-27 14:23:51 -0400

    Bug 1855742 - Part 6. Allow SourceSurfaceCanvasRecording to be created on DOM workers.
    Differential Revision: https://phabricator.services.mozilla.com/D189531

--- gfx/ipc/CanvasManagerChild.cpp
+++ gfx/ipc/CanvasManagerChild.cpp
@@ -144,6 +144,14 @@ void CanvasManagerChild::Destroy() {
   return manager;
 }
 
+/* static */ CanvasManagerChild* CanvasManagerChild::MaybeGet() {
+  if (!sLocalManager.initialized()) {
+    return nullptr;
+  }
+
+  return sLocalManager.get();
+}
+
 void CanvasManagerChild::EndCanvasTransaction() {
   if (!mCanvasChild) {
     return;
