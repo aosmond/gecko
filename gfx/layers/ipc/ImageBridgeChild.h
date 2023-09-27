bb5a5a2f3c7702edada000da1e5b5337b95c7502
Andrew Osmond <aosmond@mozilla.com>
2023-09-27 14:25:10 -0400

    Bug 1855742 - Part 3. Implement ImageBridgeChild::GetTileLockAllocator.
    This patch adds support for allocationg shmem sections for
    ImageBridgeChild. The recording infrastructure depends on it.
    
    Differential Revision: https://phabricator.services.mozilla.com/D189528

--- gfx/layers/ipc/ImageBridgeChild.h
+++ gfx/layers/ipc/ImageBridgeChild.h
@@ -20,6 +20,7 @@
 #include "mozilla/layers/PImageBridgeChild.h"
 #include "mozilla/layers/TextureForwarder.h"
 #include "mozilla/Mutex.h"
+#include "mozilla/UniquePtr.h"
 #include "mozilla/webrender/WebRenderTypes.h"
 #include "nsRegion.h"  // for nsIntRegion
 #include "mozilla/gfx/Rect.h"
@@ -156,6 +157,7 @@ class ImageBridgeChild : public PImageBridgeChild,
   void EndTransaction();
 
   uint32_t GetNamespace() const { return mNamespace; }
+  FixedSizeSmallShmemSectionAllocator* GetTileLockAllocator() override;
 
   /**
    * Returns the ImageBridgeChild's thread.
@@ -349,6 +351,7 @@ class ImageBridgeChild : public PImageBridgeChild,
   uint32_t mNextCompositableId = 0;
 
   CompositableTransaction* mTxn;
+  UniquePtr<FixedSizeSmallShmemSectionAllocator> mSectionAllocator;
 
   bool mCanSend;
   mozilla::Atomic<bool> mDestroyed;
