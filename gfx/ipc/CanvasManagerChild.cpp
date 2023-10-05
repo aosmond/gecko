f7c06d1e258fb6b1f2a24a2affb710a3fe58418e
Andrew Osmond <aosmond@mozilla.com>
2023-10-05 09:38:34 -0400

    Bug XXX - Part 1. Adding owning thread assertions to CanvasManagerChild.
    This patch just adds some new asserts for CanvasManagerChild to enforce
    the threadlocal nature of the object.

--- gfx/ipc/CanvasManagerChild.cpp
+++ gfx/ipc/CanvasManagerChild.cpp
@@ -32,6 +32,8 @@ CanvasManagerChild::CanvasManagerChild(uint32_t aId) : mId(aId) {}
 CanvasManagerChild::~CanvasManagerChild() = default;
 
 void CanvasManagerChild::ActorDestroy(ActorDestroyReason aReason) {
+  NS_ASSERT_OWNINGTHREAD(CanvasManagerChild);
+
   if (sLocalManager.get() == this) {
     sLocalManager.set(nullptr);
   }
@@ -39,6 +41,8 @@ void CanvasManagerChild::ActorDestroy(ActorDestroyReason aReason) {
 }
 
 void CanvasManagerChild::Destroy() {
+  NS_ASSERT_OWNINGTHREAD(CanvasManagerChild);
+
   if (mActiveResourceTracker) {
     mActiveResourceTracker->AgeAllGenerations();
     mActiveResourceTracker.reset();
@@ -154,6 +158,8 @@ void CanvasManagerChild::Destroy() {
 }
 
 void CanvasManagerChild::EndCanvasTransaction() {
+  NS_ASSERT_OWNINGTHREAD(CanvasManagerChild);
+
   if (!mCanvasChild) {
     return;
   }
@@ -166,6 +172,8 @@ void CanvasManagerChild::EndCanvasTransaction() {
 }
 
 void CanvasManagerChild::DeactivateCanvas() {
+  NS_ASSERT_OWNINGTHREAD(CanvasManagerChild);
+
   mActive = false;
   if (mCanvasChild) {
     mCanvasChild->Destroy();
@@ -174,6 +182,8 @@ void CanvasManagerChild::DeactivateCanvas() {
 }
 
 RefPtr<layers::CanvasChild> CanvasManagerChild::GetCanvasChild() {
+  NS_ASSERT_OWNINGTHREAD(CanvasManagerChild);
+
   if (!mActive) {
     MOZ_ASSERT(!mCanvasChild);
     return nullptr;
@@ -190,6 +200,8 @@ RefPtr<layers::CanvasChild> CanvasManagerChild::GetCanvasChild() {
 }
 
 RefPtr<webgpu::WebGPUChild> CanvasManagerChild::GetWebGPUChild() {
+  NS_ASSERT_OWNINGTHREAD(CanvasManagerChild);
+
   if (!mWebGPUChild) {
     mWebGPUChild = MakeAndAddRef<webgpu::WebGPUChild>();
     if (!SendPWebGPUConstructor(mWebGPUChild)) {
@@ -212,6 +224,8 @@ already_AddRefed<DataSourceSurface> CanvasManagerChild::GetSnapshot(
     uint32_t aManagerId, int32_t aProtocolId,
     const Maybe<RemoteTextureOwnerId>& aOwnerId, SurfaceFormat aFormat,
     bool aPremultiply, bool aYFlip) {
+  NS_ASSERT_OWNINGTHREAD(CanvasManagerChild);
+
   if (!CanSend()) {
     return nullptr;
   }
--- gfx/ipc/CanvasManagerChild.h
+++ gfx/ipc/CanvasManagerChild.h
@@ -32,6 +32,7 @@ class DataSourceSurface;
 class CanvasManagerChild final : public PCanvasManagerChild {
  public:
   NS_INLINE_DECL_THREADSAFE_REFCOUNTING(CanvasManagerChild, override);
+  NS_DECL_OWNINGTHREAD;
 
   explicit CanvasManagerChild(uint32_t aId);
   uint32_t Id() const { return mId; }
