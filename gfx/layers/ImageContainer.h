de392cd1e53b96998598f1085c8fa30e59e8b2f8
Andrew Osmond <aosmond@mozilla.com>
2023-11-15 19:57:53 -0500

    Revert "Bug 1863367 - Part 1. Add Image::BuildSurfaceDescriptorBuffer and use for RDD readback. r=gfx-reviewers,lsalzman"
    This reverts commit c5c99850ff7f6aeb22d738dee60b9fb25ab6457c.

--- gfx/layers/ImageContainer.h
+++ gfx/layers/ImageContainer.h
@@ -21,11 +21,10 @@
 #include "mozilla/layers/LayersTypes.h"  // for LayersBackend, etc
 #include "mozilla/layers/CompositorTypes.h"
 #include "mozilla/mozalloc.h"  // for operator delete, etc
-#include "mozilla/TypedEnumBits.h"
-#include "nsDebug.h"          // for NS_ASSERTION
-#include "nsISupportsImpl.h"  // for Image::Release, etc
-#include "nsTArray.h"         // for nsTArray
-#include "nsThreadUtils.h"    // for NS_IsMainThread
+#include "nsDebug.h"           // for NS_ASSERTION
+#include "nsISupportsImpl.h"   // for Image::Release, etc
+#include "nsTArray.h"          // for nsTArray
+#include "nsThreadUtils.h"     // for NS_IsMainThread
 #include "mozilla/Atomics.h"
 #include "mozilla/gfx/2D.h"
 #include "mozilla/EnumeratedArray.h"
@@ -130,15 +129,6 @@ class Image {
 
   virtual already_AddRefed<gfx::SourceSurface> GetAsSourceSurface() = 0;
 
-  enum class BuildSdbFlags : uint8_t {
-    Default = 0,
-    RgbOnly = 1 << 0,
-  };
-
-  virtual nsresult BuildSurfaceDescriptorBuffer(
-      SurfaceDescriptorBuffer& aSdBuffer, BuildSdbFlags aFlags,
-      const std::function<MemoryOrShmem(uint32_t)>& aAllocate);
-
   virtual bool IsValid() const { return true; }
 
   /**
@@ -167,12 +157,6 @@ class Image {
 
   virtual Maybe<SurfaceDescriptor> GetDesc();
 
-  static nsresult AllocateSurfaceDescriptorBufferRgb(
-      const gfx::IntSize& aSize, gfx::SurfaceFormat aFormat,
-      uint8_t*& aOutBuffer, SurfaceDescriptorBuffer& aSdBuffer,
-      int32_t& aStride,
-      const std::function<layers::MemoryOrShmem(uint32_t)>& aAllocate);
-
  protected:
   Maybe<SurfaceDescriptor> GetDescFromTexClient(
       TextureClient* tcOverride = nullptr);
@@ -198,8 +182,6 @@ class Image {
 
   static mozilla::Atomic<int32_t> sSerialCounter;
 };
-
-MOZ_MAKE_ENUM_CLASS_BITWISE_OPERATORS(Image::BuildSdbFlags)
 
 /**
  * A RecycleBin is owned by an ImageContainer. We store buffers in it that we
@@ -847,9 +829,9 @@ class PlanarYCbCrImage : public Image {
    * Build a SurfaceDescriptorBuffer with this image.  A function to allocate
    * a MemoryOrShmem with the given capacity must be provided.
    */
-  nsresult BuildSurfaceDescriptorBuffer(
-      SurfaceDescriptorBuffer& aSdBuffer, BuildSdbFlags aFlags,
-      const std::function<MemoryOrShmem(uint32_t)>& aAllocate) override;
+  virtual nsresult BuildSurfaceDescriptorBuffer(
+      SurfaceDescriptorBuffer& aSdBuffer,
+      const std::function<MemoryOrShmem(uint32_t)>& aAllocate);
 
  protected:
   already_AddRefed<gfx::SourceSurface> GetAsSourceSurface() override;
@@ -863,7 +845,7 @@ class PlanarYCbCrImage : public Image {
   gfx::IntPoint mOrigin;
   gfx::IntSize mSize;
   gfxImageFormat mOffscreenFormat;
-  RefPtr<gfx::DataSourceSurface> mSourceSurface;
+  RefPtr<gfx::SourceSurface> mSourceSurface;
   uint32_t mBufferSize;
 };
 
