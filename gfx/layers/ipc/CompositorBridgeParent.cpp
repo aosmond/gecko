0407936ce701d4df98b30228f209a545abf2224d
Andrew Osmond <aosmond@mozilla.com>
2023-11-17 12:39:15 -0500

    Bug 1854669 - Part 1.

--- gfx/layers/ipc/CompositorBridgeParent.cpp
+++ gfx/layers/ipc/CompositorBridgeParent.cpp
@@ -119,6 +119,10 @@ CompositorBridgeParentBase::~CompositorBridgeParentBase() = default;
 
 ProcessId CompositorBridgeParentBase::GetChildProcessId() { return OtherPid(); }
 
+dom::ContentParentId CompositorBridgeParentBase::GetContentId() {
+  return mCompositorManager->GetContentId();
+}
+
 void CompositorBridgeParentBase::NotifyNotUsed(PTextureParent* aTexture,
                                                uint64_t aTransactionId) {
   RefPtr<TextureHost> texture = TextureHost::AsTextureHost(aTexture);
