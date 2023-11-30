6f84bd1d1a5858e8c9c2d3f55a5bb4d1ec6bf81e
Andrew Osmond <aosmond@mozilla.com>
2023-11-30 11:24:58 -0500

    Bug 1855742 - Part 5. Refactor canvas shutdown to be synchronized with CanvasManagerChild.
    Because we cannot control the ordering of shutdown notification from
    WorkerRef, we need to choose one centralized point to begin shutdown of
    the main thread or the owning DOM worker. Since there are other objects
    owned by CanvasManagerChild, we also switch to a ThreadSafeWorkerRef.

--- dom/canvas/CanvasRenderingContext2D.h
+++ dom/canvas/CanvasRenderingContext2D.h
@@ -68,7 +68,6 @@ class Optional;
 
 struct CanvasBidiProcessor;
 class CanvasDrawObserver;
-class CanvasShutdownObserver;
 
 class DOMMatrix;
 class DOMMatrixReadOnly;
@@ -565,7 +564,7 @@ class CanvasRenderingContext2D : public nsICanvasRenderingContextInternal,
   virtual UniquePtr<uint8_t[]> GetImageBuffer(
       int32_t* out_format, gfx::IntSize* out_imageSize) override;
 
-  virtual void OnShutdown();
+  void OnShutdown();
 
   /**
    * Update CurrentState().filter with the filter description for
@@ -831,11 +830,12 @@ class CanvasRenderingContext2D : public nsICanvasRenderingContextInternal,
   // Whether the application expects to use operations that perform poorly with
   // acceleration.
   bool mWillReadFrequently = false;
+  // Whether or not we have already shutdown.
+  bool mHasShutdown = false;
 
-  RefPtr<CanvasShutdownObserver> mShutdownObserver;
-  virtual void AddShutdownObserver();
-  virtual void RemoveShutdownObserver();
-  virtual bool AlreadyShutDown() const { return !mShutdownObserver; }
+  void AddShutdownObserver();
+  void RemoveShutdownObserver();
+  bool AlreadyShutDown() const { return mHasShutdown; }
 
   /**
    * Flag to avoid duplicate calls to InvalidateFrame. Set to true whenever
