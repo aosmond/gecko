69e598afc38ccd631c7639dd2be5cad8117de37a
Ashley Zebrowski <azebrowski@mozilla.com>
2024-06-17 11:17:45 -0400

    Bug 1800882 - Implement HTMLVideoElement.requestVideoFrameCallback.

--- dom/base/AnimationFrameProvider.cpp
+++ dom/base/AnimationFrameProvider.cpp
@@ -5,6 +5,8 @@
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
 #include "mozilla/dom/AnimationFrameProvider.h"
+#include "mozilla/dom/HTMLVideoElement.h"
+#include "mozilla/dom/WorkerCommon.h"
 #include "nsThreadUtils.h"
 
 namespace mozilla::dom {
@@ -15,6 +17,10 @@ FrameRequest::FrameRequest(FrameRequestCallback& aCallback, int32_t aHandle)
 }
 
 FrameRequest::~FrameRequest() = default;
+
+FrameRequestManager::FrameRequestManager() = default;
+
+FrameRequestManager::~FrameRequestManager() = default;
 
 nsresult FrameRequestManager::Schedule(FrameRequestCallback& aCallback,
                                        int32_t* aHandle) {
@@ -30,6 +36,14 @@ nsresult FrameRequestManager::Schedule(FrameRequestCallback& aCallback,
   return NS_OK;
 }
 
+void FrameRequestManager::Schedule(HTMLVideoElement* aElement) {
+  mVideoCallbacks.AppendElement(aElement);
+}
+
+bool FrameRequestManager::Cancel(HTMLVideoElement* aElement) {
+  return mVideoCallbacks.RemoveElement(aElement);
+}
+
 bool FrameRequestManager::Cancel(int32_t aHandle) {
   // mCallbacks is stored sorted by handle
   if (mCallbacks.RemoveElementSorted(aHandle)) {
@@ -40,13 +54,32 @@ bool FrameRequestManager::Cancel(int32_t aHandle) {
   return false;
 }
 
-void FrameRequestManager::Unlink() { mCallbacks.Clear(); }
+void FrameRequestManager::Take(
+    nsTArray<RefPtr<HTMLVideoElement>>& aVideoCallbacks) {
+  MOZ_ASSERT(NS_IsMainThread());
+  aVideoCallbacks = std::move(mVideoCallbacks);
+}
+
+void FrameRequestManager::Take(nsTArray<FrameRequest>& aCallbacks) {
+  aCallbacks = std::move(mCallbacks);
+  mCanceledCallbacks.clear();
+}
+
+void FrameRequestManager::Unlink() {
+  mCallbacks.Clear();
+  mVideoCallbacks.Clear();
+}
 
 void FrameRequestManager::Traverse(nsCycleCollectionTraversalCallback& aCB) {
   for (auto& i : mCallbacks) {
     NS_CYCLE_COLLECTION_NOTE_EDGE_NAME(aCB,
                                        "FrameRequestManager::mCallbacks[i]");
     aCB.NoteXPCOMChild(i.mCallback);
+  }
+  for (auto& i : mVideoCallbacks) {
+    NS_CYCLE_COLLECTION_NOTE_EDGE_NAME(
+        aCB, "FrameRequestManager::mVideoCallbacks[i]");
+    aCB.NoteXPCOMChild(ToSupports(i));
   }
 }
 
