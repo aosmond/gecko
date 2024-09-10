5eee7b323104b666f1df39bf5e1e60b058edd98b
Andrew Osmond <aosmond@mozilla.com>
2024-09-09 21:11:08 -0400

    Bug 1908245 - Add support for missing requestVideoFrameCallback parameters.
    This patch adds support for exposing the capture time, receive time,
    processing duration and RTP timestamp parameters on the callback for
    rVFC for WebRTC related elements.
    
    It also improves the accuracy of the media time paramater for normal
    playback.

--- dom/media/VideoOutput.h
+++ dom/media/VideoOutput.h
@@ -100,8 +100,12 @@ class VideoOutput : public DirectMediaTrackListener {
         // We ignore null images.
         continue;
       }
-      images.AppendElement(ImageContainer::NonOwningImage(
-          image, chunk.mTimeStamp, frameId, mProducerID));
+      ImageContainer::NonOwningImage nonOwningImage(
+          image, chunk.mTimeStamp, frameId, mProducerID, TimeDuration::Zero(),
+          chunk.mMediaTime.IsValid() ? chunk.mMediaTime.ToSeconds() : -1.0,
+          chunk.mWebrtcCaptureTime, chunk.mWebrtcReceiveTime,
+          chunk.mRtpTimestamp);
+      images.AppendElement(std::move(nonOwningImage));
 
       lastPrincipalHandle = chunk.GetPrincipalHandle();
 
@@ -281,10 +285,7 @@ class FirstFrameVideoOutput : public VideoOutput {
 
         // Pick the first frame and run it through the rendering code.
         VideoSegment segment;
-        segment.AppendFrame(do_AddRef(c->mFrame.GetImage()),
-                            c->mFrame.GetIntrinsicSize(),
-                            c->mFrame.GetPrincipalHandle(),
-                            c->mFrame.GetForceBlack(), c->mTimeStamp);
+        segment.AppendFrame(*c);
         VideoOutput::NotifyRealtimeTrackData(aGraph, aTrackOffset, segment);
         return;
       }
--- gfx/layers/ImageTypes.h
+++ gfx/layers/ImageTypes.h
@@ -8,6 +8,9 @@
 #define GFX_IMAGETYPES_H
 
 #include <stdint.h>  // for uint32_t
+#include "mozilla/Maybe.h"
+#include "mozilla/TimeStamp.h"
+#include "mozilla/Variant.h"
 
 namespace mozilla {
 
@@ -117,6 +120,14 @@ constexpr ContainerFrameID kContainerFrameID_Invalid = 0;
 
 typedef uint32_t ContainerProducerID;
 constexpr ContainerProducerID kContainerProducerID_Invalid = 0;
+
+// int64_t represents a WebRTC NTP timestamp.
+typedef Variant<Nothing, TimeStamp, int64_t> ContainerCaptureTime;
+
+// int64_t represents a WebRTC Realtime timestamp.
+typedef Maybe<int64_t> ContainerReceiveTime;
+
+typedef Maybe<uint32_t> ContainerRtpTimestamp;
 
 }  // namespace layers
 
