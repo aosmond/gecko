ca267dd26e186b724443f37e4f943fb47123d6d3
Andrew Osmond <aosmond@mozilla.com>
2025-04-21 17:43:33 -0400

    make h264 run on gtest

--- dom/media/platforms/ffmpeg/FFmpegDecoderModule.h
+++ dom/media/platforms/ffmpeg/FFmpegDecoderModule.h
@@ -32,7 +32,7 @@ class FFmpegDecoderModule : public PlatformDecoderModule {
   }
   static void Init(FFmpegLibWrapper* aLib) {
 #if defined(MOZ_USE_HWDECODE)
-#  if (defined(XP_WIN) || defined(MOZ_WIDGET_ANDROID)) && !defined(MOZ_FFVPX_AUDIOONLY)
+#  if defined(XP_WIN) && !defined(MOZ_FFVPX_AUDIOONLY)
     if (!XRE_IsGPUProcess()) {
       return;
     }
@@ -56,6 +56,33 @@ class FFmpegDecoderModule : public PlatformDecoderModule {
           sSupportedHWCodecs.AppendElement(codecId);
           MOZ_LOG(sPDMLog, LogLevel::Debug,
                   ("Support %s on d3d11va", AVCodecToString(codecId)));
+          break;
+        }
+      }
+    }
+#  elif defined(MOZ_WIDGET_ANDROID) && !defined(MOZ_FFVPX_AUDIOONLY)
+    static nsTArray<AVCodecID> kCodecIDs({
+        AV_CODEC_ID_AV1,
+        AV_CODEC_ID_H264,
+        AV_CODEC_ID_HEVC,
+        AV_CODEC_ID_VP9,
+    });
+    for (const auto& codecId : kCodecIDs) {
+      const auto* codec =
+          FFmpegDataDecoder<V>::FindHardwareAVCodec(aLib, codecId);
+      if (!codec) {
+        MOZ_LOG(sPDMLog, LogLevel::Debug,
+                ("No codec or decoder for %s on mediacodec",
+                 AVCodecToString(codecId)));
+        continue;
+      }
+      for (int i = 0; const AVCodecHWConfig* config =
+                          aLib->avcodec_get_hw_config(codec, i);
+           ++i) {
+        if (config->methods & AV_CODEC_HW_CONFIG_METHOD_HW_DEVICE_CTX) {
+          sSupportedHWCodecs.AppendElement(codecId);
+          MOZ_LOG(sPDMLog, LogLevel::Debug,
+                  ("Support %s on mediacodec", AVCodecToString(codecId)));
           break;
         }
       }
